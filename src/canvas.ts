621a6287622bd6f019444cf8047ad90c7d58321e
Paul Adams <ghusiyy@gmail.com>
2023-02-23 20:28:55 -0800

    render black square

--- src/canvas.ts
+++ src/canvas.ts
@@ -1,1 +1,8 @@
-export function setupCanvas(element: HTMLCanvasElement) {}
+export function setupCanvas(element: HTMLCanvasElement) {
+  const ctx = element.getContext("webgl");
+  if (!ctx) {
+    return;
+  }
+  ctx.clearColor(0.0, 0.0, 0.0, 1.0);
+  ctx.clear(ctx.COLOR_BUFFER_BIT);
+}
