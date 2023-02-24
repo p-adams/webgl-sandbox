1cf029d01b9072a8d1b2444b4484754f68322dfe
Paul Adams <ghusiyy@gmail.com>
2023-02-23 20:46:45 -0800

    add gl-matrix dep

--- src/canvas.ts
+++ src/canvas.ts
@@ -1,8 +1,10 @@
+import { mat4 } from "gl-matrix";
 export function setupCanvas(element: HTMLCanvasElement) {
   const ctx = element.getContext("webgl");
   if (!ctx) {
     return;
   }
+  const _crt = mat4.create();
   ctx.clearColor(0.0, 0.0, 0.0, 1.0);
   ctx.clear(ctx.COLOR_BUFFER_BIT);
 }
